f40ff30d9667700caa05d33a3ecfd43dc8a0f5cf
altermaRRin <dev.altermarrin@gmail.com>
2021-07-05 16:12:50 +0300

    hw_5. в четвертом задании я чуть-чуть схитрила, потому что по-другому не получалось(

--- Bogdanova_MMarina_hw_5/task_2.py
+++
@@ -1,9 +0,0 @@
-# Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
-
-n = int(input('По какое число вывести нечётные числа?  '))
-
-odd_num = (num for num in range(1, n + 1) if num % 2)
-odd_num_2 = (num for num in range(1, n + 1, 2))
-
-print(*odd_num)
-print(*odd_num_2)
